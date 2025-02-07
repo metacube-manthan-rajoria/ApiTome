87e68cb5790a49e9b7feb9183189f02578ce1b90
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-02-07 15:52:46 +0530

    Added cors service

--- ApiTome/Program.cs
+++ ApiTome/Program.cs
@@ -5,14 +5,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers()
+
+//Json Formatter
 .AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.WriteIndented = true; // Enable indented formatting
 });
 
+//DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
 );
+
+//CorsHeader
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -21,6 +35,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseCors("AllowAllOrigins");
 app.UseHsts();
 app.UseHttpsRedirection();
 
