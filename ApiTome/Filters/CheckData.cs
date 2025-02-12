2aa8a22f8cbb349b54bb29010861cd1c2724e288
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-02-12 13:01:06 +0530

    removed comments

--- ApiTome/Filters/CheckData.cs
+++ ApiTome/Filters/CheckData.cs
@@ -10,28 +10,5 @@ public class DataFilter : ActionFilterAttribute
             Console.WriteLine("Invalid Model State Bro!");
             return;
         }
-
-        //bool isModelValid = filterContext.ModelState.IsValid;
-
-        // if (File.Exists(fileUrl))
-        // {
-        //     string fileContents = File.ReadAllText(fileUrl);
-        //     File.Delete(fileUrl);
-
-        //     eventLog.WriteEntry(fileContents, EventLogEntryType.SuccessAudit, 101, 1);
-        // }
-
-        // if (isModelValid)
-        // {
-        //     Log("OnActionExecuting", filterContext.RouteData);
-        // }
     }
-
-    // private void Log(string methodName, RouteData routeData)
-    // {
-    //     var controllerName = routeData.Values["controller"];
-    //     var actionName = routeData.Values["action"];
-    //     var message = String.Format("{0} controller:{1} action:{2}", methodName, controllerName, actionName);
-    //     _logger.LogInformation(message, "Action Filter Log");
-    // }
 }
